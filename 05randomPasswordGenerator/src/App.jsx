c83c2d56865c56b9f0051d35733b998c7ea6b10b
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2024-08-18 05:30:30 +0000

    intitializing variables

--- 05randomPasswordGenerator/src/App.jsx
+++ 05randomPasswordGenerator/src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 
 function App() {
   const [Length, setLength] = useState(8)
   const [Number, setNumber] = useState(false)
   const [Char, setChar] = useState(false)
-  cosnt [Password, setPassword] = useState("")
+  const [Password, setPassword] = useState("")
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (Number) str += "0123456789"
+    if (Char) str += "!@#$%^&*()_+=-{}[]~`"
+
+    for (let i = 1; i < array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+
+      pass = str.charAt(char)
+      
+    }
+
+    setPassword(pass)
+  }, [Length, Number, Char, setPassword])
+
+  
+
+
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800'>TEST</div>
     </>
   )
 }
